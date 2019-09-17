122b8d9e46823a3da4b889128164a2ce3bf727c2
smjcrane <smjcrane@gmail.com>
2019-09-17 19:07:51 +0100

    load levels from file

--- app/src/main/java/com/example/mountainclimbers/SeeMountain.java
+++ app/src/main/java/com/example/mountainclimbers/SeeMountain.java
@@ -1,39 +1,66 @@
 package com.example.mountainclimbers;
 
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class SeeMountain extends AppCompatActivity {
+
+    private MountainView mountainView;
+    public static int[] colorIDs = new int[] {R.color.climberGreen, R.color.climberPurple, R.color.climberBlue};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_mountain);
 
-        Mountain mountain = new Mountain(new int[] {0, 100, 0, 70, 40, 80, 0});
-        final MountainView mountainView = findViewById(R.id.mountainView);
-        mountainView.setMountain(mountain);
+        mountainView = findViewById(R.id.mountainView);
 
-        MountainClimber greenClimber = new MountainClimber();
-        greenClimber.setPosition(0);
-        mountainView.addClimber(greenClimber, R.color.climberGreen);
-
-        MountainClimber purpleClimber = new MountainClimber();
-        purpleClimber.setPosition(mountain.getWidth());
-        mountainView.addClimber(purpleClimber, R.color.climberPurple);
-
-        MountainClimber blueClimber = new MountainClimber();
-        blueClimber.setPosition(200);
-        mountainView.addClimber(blueClimber, R.color.climberBlue);
+        loadLevel(R.raw.lvl01);
 
         Button goButton = findViewById(R.id.mountainGoButton);
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mountainView.go();
+                SeeMountain.this.mountainView.go();
             }
         });
+    }
+
+    private void loadLevel(int levelResourceID){
+        try {
+            InputStream stream = getResources().openRawResource(levelResourceID);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+            String[] heightStrings = br.readLine().split(" ");
+            String[] climberString = br.readLine().split(" ");
+
+            int[] heights = new int[heightStrings.length];
+            for (int i = 0; i < heightStrings.length; i++) {
+                heights[i] = Integer.parseInt(heightStrings[i]);
+            }
+
+            Mountain mountain = new Mountain(heights);
+            mountainView.setMountain(mountain);
+
+            for (int i = 0; i < climberString.length; i++) {
+                MountainClimber climber = new MountainClimber();
+                climber.setPosition(Integer.parseInt(climberString[i]));
+                mountainView.addClimber(climber, colorIDs[i]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
