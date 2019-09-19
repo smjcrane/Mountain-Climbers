8a9fd9ee88e300884b2ff22178e5ce01dbc8096a
smjcrane <smjcrane@gmail.com>
2019-09-19 14:53:17 +0100

    mountains look continuous at the edges

--- app/src/main/java/com/example/mountainclimbers/MountainView.java
+++ app/src/main/java/com/example/mountainclimbers/MountainView.java
@@ -193,6 +193,12 @@ public class MountainView extends View {
         path.close();
         canvas.drawPath(path, mountainPaint);
         canvas.drawRect(0, getHeight(), getWidth(), getHeight() - PADDING, mountainPaint);
+        canvas.drawRect(0, getHeight(), PADDING,
+                 height + PADDING - mountain.getHeightAt(0) * height / mountain.getMaxHeight(),
+                mountainPaint);
+        canvas.drawRect(width + PADDING, getHeight(), getWidth(),
+                height + PADDING - mountain.getHeightAt(mountain.getWidth()) * height / mountain.getMaxHeight(),
+                mountainPaint);
     }
 
     protected void onDraw(Canvas canvas) {
